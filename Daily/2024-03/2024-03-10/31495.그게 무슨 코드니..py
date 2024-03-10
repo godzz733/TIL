5f27d4621967835a216d96673eccf4d96fe21628
godzz733 <godzz733@naver.com>
2024-03-10 21:19:11 +0900

    2024-03-10

--- Daily/2024-03/2024-03-10/31495.그게 무슨 코드니..py
+++ Daily/2024-03/2024-03-10/31495.그게 무슨 코드니..py
@@ -1,4 +1,5 @@
 t = input()
-print(len(t.strip()))
-# else:
-print(t[1:-1] if t[0] == "\"" and t[-1] == "\"" else "CE")
+if len(t[1:-1].strip()) == 0:
+    print("CE")
+else:
+    print(t[1:-1] if t[0] == "\"" and t[-1] == "\"" else "CE")
